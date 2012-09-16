ddd96d5e966dfb062ce389e284bff62ffe179d1c
badlee <badinga.ulrich@gmail.com>
2012-09-16 13:21:25 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,32 +1,56 @@
 #!/bin/sh
 
-echo "Fun_Dns setup, Do you want"
-echo "   1) Install"
-echo "   2) remove"
-echo "   3) exit"
+# Make sure only root can run our script
+if test "`id -u`" -ne 0
+then
+	echo "You need to run setup as root!"
+	exit 1
+fi
+
+echo "Fun_Dns 0.1.12 setup, Do you want"
+echo "\t1) Install"
+echo "\t2) Remove"
+echo "\t*) Exit"
 read case;
 
 case $case in
 1)
-	if [ -d /opt/fun_dns]
+	if [ -d /opt/fun_dns ]
 	then
-		rm -fr /opt/fun_dns
+		echo "An other install exist remove it and run this script!"
+		echo "For remove run"
+		echo "\t - /opt/fun_dns/setup.sh as root"
+		echo "\t - Enter 2 (for remove)"
+		echo "\t - And press ENTER"
+		exit 2
 	fi
+	echo  "Start install"
 	mkdir /opt/fun_dns
-	cd `dirname "$0"`
-	cp -r ./* /opt/fun_dns
-	#tar xvf /path/to/fun_dns.tar.gz
+	cd /opt/fun_dns
+	echo  "Get fun_dns.v0.1.12.tar.gz "
+	wget -O fun_dns.v0.1.12.tar.gz https://github.com/downloads/badlee/fun-dns/fun_dns.v0.1.12.tar.gz
+	echo "Done"
+	echo -n "Install fun_dns v0.1.12 : ";
+	tar xvf fun_dns.v0.1.12.tar.gz
+	rm fun_dns.v0.1.12.tar.gz
 	ln -s /opt/fun_dns/fun_dns.daemon /etc/init.d/fun_dns
 	update-rc.d fun_dns defaults
+	echo "Done"
 	#Start service
+	echo "Start fun_dns"
 	service fun_dns start
+	echo "fun_dns is installed"
 	;;	
 2)
+	echo "Stop fun_dns"
 	service fun_dns stop
+	echo "Uninstall fun_dns"
 	update-rc.d -f  fun_dns remove
-	rm /etc/init.d/fun_dns
-	cd /
+	rm -fr /etc/init.d/fun_dns
+	cd /var/log
+	rm -fr fun_dns.log
 	rm -fr /opt/fun_dns
+	echo "fun_dns is uninstalled"
 	;;
 3)	exit
 esac
